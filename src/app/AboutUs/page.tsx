164d3c8ecdff375d10827688d4ee1167bb1832a4
Angel Velasco <angelgg2020@outlook.com>
2024-04-24 10:01:01 -0500

    feat: :sparkles: Update about us

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -1,6 +1,9 @@
 export const AboutUs = () => {
     return (
-        <div>About us</div>
+        <>
+            <h1>About us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur distinctio ab dolorem, veritatis inventore accusantium! Totam reprehenderit quidem itaque unde quibusdam nemo dignissimos eius accusantium nisi expedita? Magni, ex.</p>
+        </>
     )
 }
 export default AboutUs
